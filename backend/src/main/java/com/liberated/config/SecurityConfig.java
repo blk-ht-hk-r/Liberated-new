1aec9544df538ac6cb144675326472824ffbce6b
Unnati Bhatia <unnatibhatia19@gmail.com>
2026-08-15 12:25:53 +0530

    import issue

--- backend/src/main/java/com/liberated/config/SecurityConfig.java
+++ backend/src/main/java/com/liberated/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.liberated.config;
 import com.liberated.auth.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
